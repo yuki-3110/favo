7f7214822113b4a5d51a749573105ad6c74adfa4
yuki saito <ykst0622@gmail.com>
2022-06-10 09:08:44 +0900

    ユーザー詳細ページ、登録後のリダイレクト作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+
   def new
     @user = User.new
   end
@@ -7,10 +7,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # 保存の成功した場合の処理
+      redirect_to user_path(@user.id)
     else
       render :new
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   private
