dde103623c3b93c5e901e6f38a1820fe94f85012
yuki saito <ykst0622@gmail.com>
2022-06-13 02:40:11 +0900

    Add platform

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'pictures#index'
+  root 'users#new'
   resources :sessions, only: %i(new create destroy)
   resources :users, only: %i(new create destroy)
   resources :pictures do
