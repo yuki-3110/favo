bc9165915c260edee15f58784c0aec461147189b
yuki saito <ykst0622@gmail.com>
2022-06-13 10:57:06 +0900

    resources :usersにshowアクションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'users#new'
   resources :sessions, only: %i(new create destroy)
-  resources :users, only: %i(new create destroy)
+  resources :users, only: %i(new create show destroy)
   resources :pictures do
     collection do
       post :confirm
